6c1bb45f2e478406f6b3af02d726a3852f505372
Dylan <alzatedylan@gmail.com>
2025-06-03 22:28:28 -0500

    push para poder traer los cambios de main

--- database/migrations/2025_05_16_000011_create_worker_table.php
+++ database/migrations/2025_05_16_000011_create_worker_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('worker', function (Blueprint $table) {
             $table->integer('worker_id')->primary();
             $table->string('name', 25);
-            $table->date('entry_date')->default(DB::raw('CURRENT_DATE'));
+            $table->date('entry_date')->nullable();
             $table->integer('salary');
             $table->integer('type_doc_id')->nullable();
             $table->integer('role_id')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,29 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', function () {
-    return view('home/index');
-})->name('home.index');
+    use App\Http\Controllers\ProfileController;
+    use Illuminate\Support\Facades\Route;
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+    Route::get('/', function () {
+        return view('home/index');
+    })->name('home.index');
 
 
-include('web/department.php');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-include('web/city.php');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-include('web/roles.php');
+    require __DIR__.'/auth.php';
+
+
+    include('web/department.php');
+
+    include('web/city.php');
+
+    include('web/roles.php');
